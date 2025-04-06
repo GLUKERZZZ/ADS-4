98cfcb49be1c77a4a803ffac2c8740afc2220d08
GLUKERZZZ <artem2801200635@gmail.com>
2025-04-06 21:23:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,66 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value) {
-  return 0;
+int countPairs1(int* arr, int len, int value) {
+  int a = 0;
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        a++;
+      }
+    }
+  }
+  return a;
 }
-int countPairs2(int *arr, int len, int value) {
-  return 0;
+int countPairs2(int* arr, int len, int value) {
+  int a = 0;
+  int end = len - 1;
+  while (end >= 0) {
+    if (arr[end] <= value) {
+      break;
+    }
+    end--;
+  }
+  int start = 0;
+  while (start < end) {
+    int x = end;
+    while (x > start) {
+      if (arr[start] + arr[x] == value) {
+        a += 1;
+      }
+      x--;
+    }
+    start++;
+  }
+  return a;
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
+int countPairs3(int* arr, int len, int value) {
+  int a = 0;
+  for (int i = 0; i < len; ++i) {
+    int first = -1, start = i + 1, end = len - 1, f = value - arr[i];
+    while (start <= end) {
+      int x = start + (end - start) / 2;
+      if (arr[x] >= f) {
+        end = x - 1;
+        if (arr[x] == f)
+          first = x;
+      } else {
+        start = x + 1;
+      }
+    }
+    if (first != -1) {
+      int last = first;
+      start = first;
+      end = len - 1;
+      while (start <= end) {
+        int x = start + (end - start) / 2;
+        if (arr[x] <= f) {
+          start = x + 1;
+          if (arr[x] == f) last = x;
+        } else {
+          end = x - 1;
+        }
+      }
+      a += last - first + 1;
+    }
+  }
+  return a;
 }
